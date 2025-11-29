b331a830cabbccd819be552dc6d2f22a72ddc0b4
Wouter De Borger <wouter.deborger@inmanta.com>
2025-11-29 11:28:31 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    info.changeScoreBy(1)
+})
 game.splash("pak de chocopotten, koop wapens en vecht.")
 tiles.setCurrentTilemap(tilemap`level1`)
 let mySprite = sprites.create(assets.image`lemming`, SpriteKind.Player)
 controller.moveSprite(mySprite)
+info.setScore(0)
 scene.cameraFollowSprite(mySprite)
