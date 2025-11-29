4c956dd500474a42b78e219889d6b2c955ad37d6
dragonpower300 <147735578+dragonpower300@users.noreply.github.com>
2025-11-29 10:20:24 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://dragonpower300.github.io/dabody-lemming/](https://dragonpower300.github.io/dabody-lemming/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/dragonpower300/dabody-lemming** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/dragonpower300/dabody-lemming** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,52 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "lemming":return img`
+.................
+..ffff.....ffff..
+.ff99ff...ff99ff.
+.f9dd9f...f9dd9f.
+.f9dd9fffff9dd9f.
+.ff99999999999ff.
+.f9999999999999f.
+ff9999999999999ff
+f9999ff999ff9999f
+f99991f9991f9999f
+f999999fff999999f
+f9999999f9999999f
+ff9999999999999ff
+.ff99999999999ff.
+..ff999999999ff..
+..f99999999999f..
+.ff99999999999ff.
+.f99999ddd99999f.
+ff9999ddddd9999ff
+f9999ddddddd9999f
+f9999ddddddd9999f
+ff99ddddddddd99ff
+.ff9ddddddddd9ff.
+..fffffffffffff..
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
